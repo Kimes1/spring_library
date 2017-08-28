e036ceebbfa3e58066df5fed6c64f2c48c369980
Niedzielski <j.niedzielski@accenture.com>
2017-08-28 16:23:48 +0200

    added configs

--- src/main/java/com/niedzielski/LibraryApplication.java
+++ src/main/java/com/niedzielski/LibraryApplication.java
@@ -1,44 +1,10 @@
 package com.niedzielski;
 
-import java.util.Properties;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class LibraryApplication {
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public JavaMailSender javaMailSender() {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost("smtp.gmail.com");
-		mailSender.setPort(587);
-		mailSender.setUsername("springwebapplication@gmail.com");
-		mailSender.setPassword("nowehaslo");
-
-		Properties props = mailSender.getJavaMailProperties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.debug", "true");
-
-		return mailSender;
-	}
-
-	@Bean
-	public SimpleMailMessage message() {
-		return new SimpleMailMessage();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
---
+++ src/main/java/com/niedzielski/config/GlobalConfig.java
@@ -0,0 +1,14 @@
+package com.niedzielski.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class GlobalConfig {
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
