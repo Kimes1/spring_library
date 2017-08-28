c4db99fc7117026a79889abdc19ba2517d449595
Niedzielski <j.niedzielski@accenture.com>
2017-08-28 17:30:24 +0200

    added EmailScheduler

--- src/main/java/com/niedzielski/repository/BookRepository.java
+++ src/main/java/com/niedzielski/repository/BookRepository.java
@@ -1,5 +1,7 @@
 package com.niedzielski.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.niedzielski.model.Book;
@@ -13,5 +15,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 	public Book findOneByIsbnAndStatus(Long isbn, Status status);
 
 	public Book findOneByIsbnAndUser(Long isbn, User user);
+
+	public List<Book> findByStatus(Status status);
 
 }
---
+++ src/main/java/com/niedzielski/schedule/EmailScheduler.java
@@ -0,0 +1,42 @@
+package com.niedzielski.schedule;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.niedzielski.model.Book;
+import com.niedzielski.model.Book.Status;
+import com.niedzielski.repository.BookRepository;
+import com.niedzielski.service.EmailService;
+
+@Component
+public class EmailScheduler {
+
+	private final BookRepository bookRepository;
+	private final EmailService emailService;
+
+	@Autowired
+	public EmailScheduler(BookRepository bookRepository, EmailService emailService) {
+		this.bookRepository = bookRepository;
+		this.emailService = emailService;
+	}
+
+	@Scheduled(cron = "0 0 12 1/1 * ? *")
+	public void sendReminders() {
+		List<Book> books = bookRepository.findByStatus(Status.NON_AVAILABLE);
+		for (Book book : books) {
+			LocalDate returnDate = book.getReturnDate();
+			LocalDate currentDate = LocalDate.now();
+			String subject = "Reminder";
+			String message = "Please return your book";
+			if (returnDate.plusDays(3).isAfter(currentDate)) {
+				emailService.sendSimpleMessage(book.getUser().getEmail(), subject, message);
+			}
+
+		}
+	}
+
+}
